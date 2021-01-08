0feaf9ae052fc59b8b38693a735898c88a664ed9
MaxSerbin <silverspoor644@gmail.com>
2021-01-08 20:41:53 +0300

    1.5. Поля объекта [#406960]

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+public class Cat {
+    private String name;
+    private String food;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
+    public static void main(String[] args) {
+        Cat gav = new Cat();
+        gav.giveNick("Гав");
+        gav.eat("котлета");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("Черный");
+        black.eat("рыба");
+        black.show();
+    }
+}
