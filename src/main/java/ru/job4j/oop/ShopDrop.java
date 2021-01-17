1ca013999858db2dbb88c8fb678a165664e05718
MaxSerbin <silverspoor644@gmail.com>
2021-01-17 07:33:06 +0300

    3.2. Удаление моделей из массива. [#406986]

--- src/main/java/ru/job4j/oop/ShopDrop.java
+++ src/main/java/ru/job4j/oop/ShopDrop.java
@@ -2,14 +2,10 @@ package ru.job4j.oop;
 
 public class ShopDrop {
     public static Product[] leftShift(Product[] products, int index) {
-        for (int i = 0; i < products.length - 1; i++) {
-            if(index <= 0) {
+        for (int i = index; i < products.length - 1; i++) {
                 products[i] = products[i + 1];
-                products[i + 1] = null;
-            } else {
-                products[index] = null;
-            }
         }
-        return products;
+        products[products.length - 1] = null;
+           return products;
     }
 }
