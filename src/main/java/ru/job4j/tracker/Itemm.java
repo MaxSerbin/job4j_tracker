c25d61d917d92eca84fd214ba76f205c9d41f245
MaxSerbin <silverspoor644@gmail.com>
2021-01-20 05:43:35 +0300

    9. toString [#406987]

--- src/main/java/ru/job4j/tracker/Itemm.java
+++ src/main/java/ru/job4j/tracker/Itemm.java
@@ -12,9 +12,7 @@ public class Itemm {
     private int id;
         private String name;
 
-        public int getId() {
-            return id;
-        }
+        public int getId() { return id; }
 
         public void setId(int id) {
             this.id = id;
---
+++ src/main/java/ru/job4j/tracker/NewStartUI.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker;
+
+public class NewStartUI {
+    public static void main(String[] args) {
+        Tracker tracker = new Tracker();
+        Itemm solution = new Itemm();
+        solution.setName("One");
+        solution.setId(1);
+        tracker.add(solution);
+        System.out.println(solution);
+
+    }
+}
