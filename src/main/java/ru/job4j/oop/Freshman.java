6cec6c7c896ae54ea9bd35c6f6c5162e14801c9d
MaxSerbin <silverspoor644@gmail.com>
2021-01-22 09:22:37 +0300

    3.1. Приведение типов. Повышение типа и понижение типа. [#406913]

---
+++ src/main/java/ru/job4j/oop/Freshman.java
@@ -0,0 +1,8 @@
+package ru.job4j.oop;
+
+public class Freshman extends Student1 {
+    @Override
+    public String toString() {
+        return "Freshman{}";
+    }
+}
---
+++ src/main/java/ru/job4j/oop/Student1.java
@@ -0,0 +1,8 @@
+package ru.job4j.oop;
+
+public class Student1 extends College1 {
+    @Override
+    public String toString() {
+        return "Student1{}";
+    }
+}
