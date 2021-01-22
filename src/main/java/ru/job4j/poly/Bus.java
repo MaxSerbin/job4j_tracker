298549fb8a132ad6062d27f2997e3eb302827c73
MaxSerbin <silverspoor644@gmail.com>
2021-01-22 08:29:19 +0300

    3. Интерфейс Input [#406910]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,21 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+
+    @Override
+    public void go() {
+        System.out.println("Go!");
+    }
+
+    @Override
+    public void passengers(int passenger) {
+        System.out.println("Passengers are: " + passenger);
+    }
+
+    @Override
+    public int refuel(int fuel) {
+       int priceByLitre = 5;
+       int rsl = fuel * priceByLitre;
+        return rsl;
+    }
+}
