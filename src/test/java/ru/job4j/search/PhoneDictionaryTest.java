fbc349a588d67dfc184dea83b31ed79f2afda214
MaxSerbin <silverspoor644@gmail.com>
2021-03-22 19:31:53 +0300

    1. Телефонный справочник на базе ArrayList [#406887]

---
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+
+    @Test
+    public void find() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(new Person("Max","Serbin","111333","Moscow"));
+        ArrayList<Person> persons = phones.find("Max");
+        assertThat(persons.get(0).getSurname(), is("Serbin"));
+    }
+
+}
