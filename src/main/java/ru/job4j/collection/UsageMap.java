d801027c18ef9a9327d058245c487438acb237a1
MaxSerbin <silverspoor644@gmail.com>
2021-03-30 20:09:18 +0300

    0. Map, HashMap [#406895]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Serbin@gmail.com", "Max Serbin");
+        map.put("Serbin1@mail.ru", "Max Serbin");
+        map.put("Maxxx@yandex.ru", "Max Serbin");
+        for(String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " " + value);
+        }
+    }
+}
