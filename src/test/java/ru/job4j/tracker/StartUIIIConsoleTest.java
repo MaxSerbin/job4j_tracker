18f2f44c96a327f3da49c3898cb469a69ae01ae9
MaxSerbin <silverspoor644@gmail.com>
2021-04-08 21:35:22 +0300

    9.2. Тесты вывода на консоль [#406907]

--- src/test/java/ru/job4j/tracker/StartUIIIConsoleTest.java
+++ src/test/java/ru/job4j/tracker/StartUIIIConsoleTest.java
@@ -27,49 +27,48 @@ public class StartUIIIConsoleTest {
         Output out = new StubOutput();
         Input in = new StubInput(new String[]{"0", "1"});
         Tracker tracker = Tracker.getTracker();
-        Itemm itemm = tracker.add(new Itemm("AAA"));
         UserAction[] actions = {new ShowAllAction(out), new ExitAction()};
         new StartUIII(out).init(in, tracker, Arrays.asList(actions));
         assertThat(out.toString(), is("Menu:" + System.lineSeparator()
                 + "0. === Show all Itemm ==="
                 + System.lineSeparator() + "1. === EXIT ===" + System.lineSeparator()
-                + "=== Show All ===" + System.lineSeparator() + itemm + System.lineSeparator()
+                + "=== Show All ===" + System.lineSeparator() + "Itemm{id=1, name='AAA'}"
+                + System.lineSeparator()
                 + "Menu:" + System.lineSeparator()
                 + "0. === Show all Itemm ===" + System.lineSeparator() + "1. === EXIT ==="
                 + System.lineSeparator()));
     }
 
- //   @Test
- //   public void whenFindById() {
- //       Output out = new StubOutput();
- //       Input in = new StubInput(new String[]{"0", "1", "1"});
- //       Tracker tracker = Tracker.getTracker();
- //       Itemm itemm = tracker.add(new Itemm("AAA"));
- //       UserAction[] actions = {new FindByIdAction(out), new ExitAction()};
- //       new StartUIII(out).init(in, tracker, Arrays.asList(actions));
- //       assertThat(out.toString(), is("Menu:" + System.lineSeparator()
- //               + "0. === Find itemm by Id ==="
- //               + System.lineSeparator() + "1. === EXIT ===" + System.lineSeparator()
- //               + "=== Find by Id ===" + System.lineSeparator() + itemm
- //               + System.lineSeparator() + "Menu:" + System.lineSeparator()
- //               + "0. === Find itemm by Id ===" + System.lineSeparator() + "1. === EXIT ==="
- //               + System.lineSeparator()));
- //   }
+    @Test
+    public void whenFindById() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[]{"0", "1", "1"});
+        Tracker tracker = Tracker.getTracker();
+        Itemm itemm = tracker.add(new Itemm("AAA"));
+        UserAction[] actions = {new FindByIdAction(out), new ExitAction()};
+        new StartUIII(out).init(in, tracker, Arrays.asList(actions));
+        assertThat(out.toString(), is("Menu:" + System.lineSeparator()
+                + "0. === Find itemm by Id ==="
+                + System.lineSeparator() + "1. === EXIT ===" + System.lineSeparator()
+                + "=== Find by Id ===" + System.lineSeparator() + itemm
+                + System.lineSeparator() + "Menu:" + System.lineSeparator()
+                   + "0. === Find itemm by Id ===" + System.lineSeparator() + "1. === EXIT ==="
+                + System.lineSeparator()));
+    }
 
-//    @Test
-//    public void whenFindByName() {
-//        Output out = new StubOutput();
-//        Input in = new StubInput(new String[]{"0", "AAA", "1"});
-//        Tracker tracker = Tracker.getTracker();
-//        Itemm itemm = tracker.add(new Itemm("AAA"));
-//        UserAction[] actions = {new FindByNameAction(out), new ExitAction()};
-//        new StartUIII(out).init(in, tracker, Arrays.asList(actions));
-//        assertThat(out.toString(), is("Menu:" + System.lineSeparator()
-//                + "0. === Find itemms by name ==="
-//                + System.lineSeparator() + "1. === EXIT ===" + System.lineSeparator()
-//                + "=== Find by name ===" + System.lineSeparator() + itemm
-//                + System.lineSeparator() + "Menu:" + System.lineSeparator()
-//                + "0. === Find itemms by name ===" + System.lineSeparator() + "1. === EXIT ==="
-//                + System.lineSeparator()));
-//    }
+    @Test
+    public void whenFindByName() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[]{"0", "AAA", "1"});
+        Tracker tracker = Tracker.getTracker();
+        UserAction[] actions = {new FindByNameAction(out), new ExitAction()};
+        new StartUIII(out).init(in, tracker, Arrays.asList(actions));
+        assertThat(out.toString(), is("Menu:" + System.lineSeparator()
+                + "0. === Find itemms by name ==="
+                + System.lineSeparator() + "1. === EXIT ===" + System.lineSeparator()
+                + "=== Find by name ===" + System.lineSeparator() + "Itemm{id=1, name='AAA'}"
+               + System.lineSeparator() + "Menu:" + System.lineSeparator()
+                + "0. === Find itemms by name ===" + System.lineSeparator() + "1. === EXIT ==="
+                + System.lineSeparator()));
+    }
 }
